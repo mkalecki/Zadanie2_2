cdf5dff332f0492c3ba32feee390aa239e6a302e
Michał Kalecki <mkalecki@gmail.com>
2018-07-05 17:12:41 +0200

    poprawione na printf

--- src/Cars.java
+++ src/Cars.java
@@ -4,10 +4,17 @@ public class Cars {
         Car car2 = new Car ("Opel", "Vectra", 2004, 1.8);
 
         System.out.println("Samochód 1:");
-        System.out.println("Marka: " + car1.brand + ", model: " + car1.model +
-                ", rocznik: " + car1.year + ", pojemność: " + car1.capacity);
+
+        System.out.printf("Marka: %s, model %s, rocznik %s, pojemność %s\n",car1.brand,
+                car1.model, car1.year,car1.capacity);
+
         System.out.println("Samochód 2:");
-        System.out.println("Marka: " + car2.brand + ", model: " + car2.model +
-                ", rocznik: " + car2.year + ", pojemność: " + car2.capacity);
+
+        System.out.printf("Marka: %s, model %s, rocznik %s, pojemność %s\n",car2.brand,
+                car2.model, car2.year,car2.capacity);
+
+
+
+
     }
 }
