0fbe5d1ab10bf28e38c64fb5c2dd56df1b6c451e
Michał Kalecki <mkalecki@gmail.com>
2018-07-05 16:12:32 +0200

    zadanie1

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+class Car {
+    String brand;
+    String model;
+    int year;
+    double capacity;
+
+    Car (String br, String mo, int ye, double ca) {
+        brand = br;
+        model = mo;
+        year = ye;
+        capacity = ca;
+    }
+
+}
